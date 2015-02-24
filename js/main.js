7d1bfa6e5fe79ff0b7caefbf3b85cf9293919f19
devonash <dash@Ds-MacBook-Pro.local>
2015-02-23 20:06:20 -0500

    Added Javascript

---
+++ js/main.js
@@ -0,0 +1,54 @@
+var blueberries = {
+    name: 'blueberry',
+    firstName: "Vaccinium corymbosum",
+    growsOnShrub: true,
+    energy: 240,
+    carbohydrates: 14.49,
+    protein: 0.74,
+};
+
+var grapes = {
+    firstName: "Vitis vinifera",
+    growsOnShrub: false,
+    energy: 288,
+    carbohydrates: 18.1,
+    protein: 0.72,
+};
+
+var redCurrant = {
+    firstName: "Ribes rubrum",
+    growsOnShrub: true,
+    energy: 234,
+    carbohydrates: 13.8,
+    protein: 1.4,
+};
+
+
+
+var writeBerryInfo = function (berry) {
+    document.write('<h1>' + berry.firstName + '</h1>');
+    
+    document.write('<dl>');
+    
+    document.write('<dt>Grows on Shrub</dt>');
+    document.write('<dd>'+ berry.growsOnShrub + '</dd>');
+    
+    document.write('<dt>Energy</dt>');
+    document.write('<dd>'+ berry.energy + ' kJ</dd>');
+    
+    document.write('<dt>Carbohydrates</dt>');
+    document.write('<dd>'+ berry.carbohydrates + ' g</dd>');
+    
+    document.write('<dt>Protein</dt>');
+    document.write('<dd>'+ berry.protein + ' g</dd>');
+    
+    document.write('</dl>');
+    
+};
+
+
+
+var allTheBerries = [blueberries, grapes, redCurrant];
+
+allTheBerries.forEach(writeBerryInfo);
+
